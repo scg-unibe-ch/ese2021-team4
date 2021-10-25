b1424e6036d96a114f5dbbff848f9cc4c36991b9
Nina Frasa <nina.frasa@students.unibe.ch>
2021-10-25 21:45:33 +0200

    changed crud for posts

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/post-feed/post/post.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/post-feed/post/post.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from 'src/app/models/post.model';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
+import { environment } from 'src/environments/environment';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  loggedIn: boolean | undefined;
 
+  user: User | undefined;
+
+  constructor(
+    public httpClient: HttpClient,
+    public userService: UserService
+  ) {
+    // Listen for changes
+    userService.loggedIn$.subscribe(res => this.loggedIn = res);
+    userService.user$.subscribe(res => this.user = res);
+
+    // Current value
+    this.loggedIn = userService.getLoggedIn();
+    this.user = userService.getUser();
+  }
   ngOnInit(): void {
   }
 
+  @Input()
+  post: Post = new Post(0, '', 0, '', 0, '');
+
+  @Output()
+    update = new EventEmitter<Post>();
+
+  @Output()
+    delete = new EventEmitter<Post>();
+
+    updatePost(): void {
+      // Emits event to parent component that TodoItem got updated
+      this.update.emit(this.post);
+    }
+
+    deletePost(): void {
+      // Emits event to parent component that TodoItem got deleted
+      this.delete.emit(this.post);
+    }
 }

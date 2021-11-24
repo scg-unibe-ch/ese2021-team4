33d26666f0feeb0eaeb688e2f258bef4248e6989
Nina Frasa <nina.frasa@students.unibe.ch>
2021-11-24 16:45:49 +0100

    fixed order shipped/cancelled status after refreshing page

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/fan-shop/order/order.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/fan-shop/order/order.component.ts
@@ -4,6 +4,7 @@ import {UserService} from "src/app/services/user.service";
 import {User} from "src/app//models/user.model";
 import {Status} from 'src/app/models/status.model';
 import {HttpClient} from "@angular/common/http";
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-order',
@@ -15,6 +16,7 @@ export class OrderComponent implements OnInit {
   user: User|undefined;
 
   loggedIn: boolean|undefined;
+
 
   constructor(
     public httpClient: HttpClient,
@@ -36,19 +38,57 @@ export class OrderComponent implements OnInit {
   @Input()
   order: Order = new Order(Status.Pending, 0, 0, 0, 0, new Date(), new Date(), '', '', '', 0, 0, '', '');
 
+  @Input()
+  isShipped: boolean = false;
+
   @Output()
   update = new EventEmitter<Order>();
 
+
   cancelOrder(): void {
-    this.order.status = Status.Cancelled;
+    this.httpClient.put(environment.endpointURL + "order/" + this.order.orderId, {
+      status: Status.Cancelled,
+      orderId: this.order.orderId,
+      userId: this.order.userId,
+      productId: this.order.productId,
+      adminId: this.user!.userId,
+      createdDate: this.order.createdDate,
+      shippedDate: new Date(),
+
+      orderFirstName: this.order.orderFirstName,
+      orderLastName: this.order.orderLastName,
+      orderStreet: this.order.orderStreet,
+      orderHouseNr: this.order.orderHouseNr,
+      orderZipCode: this.order.orderZipCode,
+      orderCity: this.order.orderCity,
+      orderPhoneNr: this.order.orderPhoneNr,
+    }).subscribe();
     this.update.emit(this.order);
   }
 
   shipOrder(): void {
     if(this.user?.isAdmin) {
-      this.order.status = Status.Shipped;
-      this.order.adminId = this.user!.userId;
+      this.isShipped = true;
+      this.httpClient.put(environment.endpointURL + "order/" + this.order.orderId, {
+        status: Status.Shipped,
+        orderId: this.order.orderId,
+        userId: this.order.userId,
+        productId: this.order.productId,
+        adminId: this.user!.userId,
+        createdDate: this.order.createdDate,
+        shippedDate: new Date(),
+
+        orderFirstName: this.order.orderFirstName,
+        orderLastName: this.order.orderLastName,
+        orderStreet: this.order.orderStreet,
+        orderHouseNr: this.order.orderHouseNr,
+        orderZipCode: this.order.orderZipCode,
+        orderCity: this.order.orderCity,
+        orderPhoneNr: this.order.orderPhoneNr,
+      }).subscribe();
+
       this.update.emit(this.order);
     }
+
   }
 }

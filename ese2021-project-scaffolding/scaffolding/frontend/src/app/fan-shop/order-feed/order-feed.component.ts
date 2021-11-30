28eddda5dc5e0dc6dffaf9f261bd671c8f1a2c07
marulila <bobst.marula@gmail.com>
2021-11-30 10:49:15 +0100

    added order-feed component files to git

---
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/fan-shop/order-feed/order-feed.component.ts
@@ -0,0 +1,72 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from "../../models/user.model";
+import {HttpClient} from "@angular/common/http";
+import {UserService} from "../../services/user.service";
+import {Order} from "../../models/order.model";
+import {StatusFinder} from "../../models/status.model";
+import {environment} from "../../../environments/environment";
+
+@Component({
+  selector: 'app-order-feed',
+  templateUrl: './order-feed.component.html',
+  styleUrls: ['./order-feed.component.css']
+})
+export class OrderFeedComponent implements OnInit {
+
+  loggedIn: boolean | undefined;
+
+  user: User | undefined;
+
+  @Input()
+  feedType!: string;
+
+  orders: Order[] = [];
+  selectedOrders: Order[] = this.orders;
+  selectedStatus = 'all';
+
+  constructor(
+    public httpClient: HttpClient,
+    public userService: UserService
+  ) {
+    // Listen for changes
+    userService.loggedIn$.subscribe(res => this.loggedIn = res);
+    userService.user$.subscribe(res => {this.user = res; this.getOrders()});
+
+    // Current value
+    this.loggedIn = userService.getLoggedIn();
+    this.user = userService.getUser();
+  }
+
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders(): void {
+    if (this.feedType == 'admin') {
+      this.httpClient.get(environment.endpointURL + 'order/').subscribe((orders: any) => {
+        orders.forEach((order: any) => this.orders.push(new Order(StatusFinder.status(order.status), order.orderId, order.userId, order.productId, order.adminId, new Date(order.createdAt), new Date(order.shippedDate), order.orderFirstName, order.orderLastName, order.orderStreet, order.orderHouseNr, order.orderZipCode, order.orderCity, order.orderPhoneNr)));
+        this.selectedOrders = this.orders;
+      })
+    } else {
+      this.httpClient.get(environment.endpointURL + 'order/createdBy/' + this.user?.userId).subscribe((orders: any) => {
+        orders.forEach((order: any) => this.orders.push(new Order(StatusFinder.status(order.status), order.orderId, order.userId, order.productId, order.adminId, new Date(order.createdAt), new Date(order.shippedDate), order.orderFirstName, order.orderLastName, order.orderStreet, order.orderHouseNr, order.orderZipCode, order.orderCity, order.orderPhoneNr)));
+        this.selectedOrders = this.orders;
+      })
+    }
+  }
+
+  filterBy(status: string): void {
+    if(status == 'all'){
+      this.selectedOrders = this.orders;
+    } else {
+      this.selectedOrders = this.orders.filter(order => order.status == StatusFinder.status(status))
+    }
+  }
+
+  updateOrder(order: Order): void {
+    this.httpClient.put(environment.endpointURL + "order/" + order.orderId, {
+      status: order.status
+    })
+  }
+
+}

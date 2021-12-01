4820b41c562384e4a47ffdc52d564ed64af89882
marulila <bobst.marula@gmail.com>
2021-12-01 14:51:58 +0100

    added choice between payment by invoice or stripe

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/app.module.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {ToastrModule} from "ngx-toastr";
-import{MatRadioModule} from '@angular/material/radio';
+import {MatRadioModule} from '@angular/material/radio';
 
 //import { TodoListComponent } from './todo-list/todo-list.component';
 // import { TodoItemComponent } from './todo-list/todo-item/todo-item.component';
@@ -84,32 +84,33 @@ const routes: Routes = [
     UserListComponent,
     LoginFeedbackComponent
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        ToastrModule.forRoot(),
-        NgxAwesomePopupModule.forRoot(),
-        ConfirmBoxConfigModule.forRoot(),
-        HttpClientModule,
-        MatToolbarModule,
-        MatTabsModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        MatListModule,
-        FormsModule,
-        MatCheckboxModule,
-        RouterModule.forRoot(routes),
-        MatIconModule,
-        MatSelectModule,
-        AngularEditorModule,
-        ReactiveFormsModule,
-        MatGridListModule,
-        MatChipsModule,
-        NgxStripeModule.forRoot('pk_test_51K0pOrIFU9ZBSiczIlnqN7V1M1A7mf0vb6KDUJR01ajD8j3Lfht3SPgImeSnweRtNr29wjknKCEn2gGd4Zwha6Bx00VKqhuUP6')
-        // Component
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    NgxAwesomePopupModule.forRoot(),
+    ConfirmBoxConfigModule.forRoot(),
+    HttpClientModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    FormsModule,
+    MatCheckboxModule,
+    RouterModule.forRoot(routes),
+    MatIconModule,
+    MatSelectModule,
+    AngularEditorModule,
+    ReactiveFormsModule,
+    MatGridListModule,
+    MatChipsModule,
+    NgxStripeModule.forRoot('pk_test_51K0pOrIFU9ZBSiczIlnqN7V1M1A7mf0vb6KDUJR01ajD8j3Lfht3SPgImeSnweRtNr29wjknKCEn2gGd4Zwha6Bx00VKqhuUP6'),
+    MatRadioModule,
+    // Component
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,

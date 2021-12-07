8a26698ec72de064648c2138435a873e7af6b74c
marulila <bobst.marula@gmail.com>
2021-12-07 11:15:38 +0100

    added confirm messages for flagging/unflagging posts

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/post-feed/post/post.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/post-feed/post/post.component.ts
@@ -11,6 +11,8 @@ import {Comment} from 'src/app/models/comment.model';
 import {Category, CategoryFinder} from "../../models/category.model";
 import {ConfirmBoxInitializer, DialogLayoutDisplay} from "@costlydeveloper/ngx-awesome-popup";
 import {ToastrService} from "ngx-toastr";
+import {retry} from "rxjs/operators";
+import {Subscription} from "rxjs";
 
 
 @Component({
@@ -231,7 +233,7 @@ export class PostComponent implements OnInit {
             this.post.images.push(picture);
             img.onload = function() {
               URL.revokeObjectURL(img.src);
-            }
+            };
             imageSpan?.appendChild(img);
            });
         });
@@ -339,97 +341,119 @@ export class PostComponent implements OnInit {
 
 
   confirmDeleting(post: Post): void{
+    this.confirm('Are you sure you want to delete this post?')
+      .then(confirmed => {
+        if(confirmed){
+          this.deletePost(post);
+        }
+      })
+  }
+
+  confirmFlagging(): void{
+    this.confirm('Are you sure you want to report this post? This cannot be undone.')
+      .then(confirmed => {
+        if(confirmed){
+          this.flagPost();
+        }
+      })
+  }
+
+  confirmUnflagging(): void{
+    this.confirm('Are you sure you want to remove all flags from this post?')
+      .then(confirmed => {
+        if(confirmed){
+          this.unflagPost();
+        }
+      })
+  }
+
+  async confirm(message: string): Promise<boolean> {
     const confirmBox = new ConfirmBoxInitializer();
     confirmBox.setTitle('');
-    confirmBox.setMessage('Are you sure you want to delete this post?');
+    confirmBox.setMessage(message);
     confirmBox.setButtonLabels('YES', 'NO');
 
     // Choose layout color type
     confirmBox.setConfig({
       LayoutType: DialogLayoutDisplay.WARNING// SUCCESS | INFO | NONE | DANGER | WARNING
     });
-
-    // Simply open the popup and listen which button is clicked
-    confirmBox.openConfirmBox$().subscribe(resp => {
-
-      if (resp.ClickedButtonID=='yes'){
-        this.deletePost(post);
-      }
-    });
+    return await confirmBox.openConfirmBox$().toPromise().then(resp => resp.ClickedButtonID == 'yes')
   }
 
   votePost(param: number): void {
     if (!this.hasDownvoted && !this.hasUpvoted) {
       if (!this.hasFlagged) {
+        // create new userpostvote entry
         this.httpClient.post(environment.endpointURL + "userpostvote", {
           userId: this.user?.userId,
           postId: this.post.postId,
           vote: param
-        }).subscribe((vote: any) => {
-          if (param == 1) {
-            this.post.upvotes += 1;
-            this.hasUpvoted = true;
-          } else {
-            this.post.downvotes += 1;
-            this.hasDownvoted = true;
-          }
-          this.updatePost(this.post);
-          this.checkVoteStatus();
-        }, error => {
-          console.log(error);
-        });
-      } else {
+        })
+          .subscribe((vote: any) => {
+            this.setVote(param);
+            }, error => {
+            console.log(error);
+          });
+      }
+      else {
+        // update existing userpostvote entry
         this.httpClient.put(environment.endpointURL + "userpostvote/" + this.user?.userId + "/" + this.postId, {
           userId: this.user?.userId,
           postId: this.post.postId,
           vote: param
-        }).subscribe((vote: any) => {
-          if (param == 1) {
-            this.post.upvotes += 1;
-            this.hasUpvoted = true;
-          } else {
-            this.post.downvotes += 1;
-            this.hasDownvoted = true;
-          }
-          this.updatePost(this.post);
-          this.checkVoteStatus();
-        }, error => {
-          console.log(error);
-        });
-      }
-    }
-      else if (!this.hasFlagged) {
-          this.httpClient.delete(environment.endpointURL + "userpostvote/single/" + this.user?.userId + "/" + this.postId).subscribe(() => {
-            if (param == 1) {
-              this.post.upvotes = this.post.upvotes - 1;
-              this.hasUpvoted = false;
-            } else {
-              this.post.downvotes = this.post.downvotes -1;
-              this.hasDownvoted = false;
-            }
-            this.updatePost(this.post);
-            this.checkVoteStatus();
-          });
-        } else {
-          this.httpClient.put(environment.endpointURL + "userpostvote/" + this.user?.userId + "/" + this.postId, {
-            userId: this.user?.userId,
-            postId: this.post.postId,
-            vote: null
-          }).subscribe((vote: any) => {
-            if (param == 1) {
-              this.post.upvotes -= 1;
-              this.hasUpvoted = false;
-            } else {
-              this.post.downvotes -= 1;
-              this.hasDownvoted = false;
-            }
-            this.updatePost(this.post);
-            this.checkVoteStatus();
-          }, error => {
+        })
+          .subscribe((vote: any) => {
+            this.setVote(param);
+            }, error => {
             console.log(error);
           });
-        }
+      }
     }
+    else if (!this.hasFlagged) {
+      this.httpClient.delete(environment.endpointURL + "userpostvote/single/" + this.user?.userId + "/" + this.postId).subscribe(() => {
+        this.removeVote(param);
+      });
+    }
+    else {
+      this.httpClient.put(environment.endpointURL + "userpostvote/" + this.user?.userId + "/" + this.postId, {
+        userId: this.user?.userId,
+        postId: this.post.postId,
+        vote: null
+      })
+        .subscribe((vote: any) => {
+          this.removeVote(param);
+          }, error => {
+          console.log(error);
+        });
+    }
+  }
+
+  setVote(param: number): void {
+    if (param == 1) {
+      this.post.upvotes += 1;
+      this.hasUpvoted = true;
+    }
+    else {
+      this.post.downvotes += 1;
+      this.hasDownvoted = true;
+    }
+    this.updatePost(this.post);
+    this.checkVoteStatus();
+  }
+
+  removeVote(param: number): void {
+    if (param == 1) {
+      this.post.upvotes = this.post.upvotes - 1;
+      this.hasUpvoted = false;
+    }
+    else {
+      this.post.downvotes = this.post.downvotes -1;
+      this.hasDownvoted = false;
+    }
+    this.updatePost(this.post);
+    this.checkVoteStatus();
+  }
+
 
   // CREATE - Comment
   createComment(): void {
@@ -485,9 +509,12 @@ export class PostComponent implements OnInit {
         this.httpClient.put(environment.endpointURL + "post/" + this.post.postId, {
           flags: 0
         })
-          .subscribe(() => this.toastr.success("The flag count has been reset.","",{
-              timeOut: 2500
-            })
+          .subscribe(() => {
+              this.toastr.success("The flag count has been reset.", "", {
+                timeOut: 2500
+              });
+              this.checkVoteStatus();
+            }
           )}, error => console.log(error));
   }
 

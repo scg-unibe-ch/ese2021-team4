4ec6ddc206d8990b60de1126d614ff43dd1e569f
brateboi <florin.achermann@gmx.ch>
2021-12-12 01:55:32 +0100

    styled postfeed, only in main. Has minor things not done yet

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/models/post.model.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/models/post.model.ts
@@ -2,6 +2,9 @@ import { Comment } from 'src/app/models/comment.model';
 import { Category } from 'src/app/models/category.model';
 
 export class Post {
+    public neededRowspan: number;
+    public timeSince: String;
+    public noHtmlDescription: String;
 
     constructor(
         public postId: number,
@@ -15,5 +18,57 @@ export class Post {
         public comments: Comment[],
         public images: File[],
         public flags: number,
-    ) {}
+    ) {
+        //calculate needed rows for displaying in post-feed
+        this.timeSince = this.calculateTimeSince(this.createdAt);
+        this.noHtmlDescription = this.removeTags(this.description);
+        this.neededRowspan = this.calculateNeededRowspan();
+    }
+
+    calculateNeededRowspan() : number {
+        var titleLines = Math.ceil(this.title.length / 60);
+        var descriptionLines = Math.min(2 ,Math.ceil(this.noHtmlDescription.length / 100));
+        var minimumRows = 1;
+        return titleLines + descriptionLines + minimumRows;
+    }
+
+    removeTags(description : String) : String {
+        // var test = "etas <as> asj <149> jadglh <234h>"
+        var result = description;
+        while (result.includes("<") && result.includes(">")){
+            result = result.replace(/<.*?>/, "");
+
+        }
+        // console.log(result);
+        return result;
+    }
+
+    calculateTimeSince(date : Date) : String {
+        var seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
+
+        var interval = seconds / 31536000;
+
+        if (interval > 1) {
+            return Math.floor(interval) + " years";
+        }
+        interval = seconds / 2592000;
+        if (interval > 1) {
+            return Math.floor(interval) + " months";
+        }
+        interval = seconds / 86400;
+        if (interval > 1) {
+            return Math.floor(interval) + " days";
+        }
+        interval = seconds / 3600;
+        if (interval > 1) {
+            return Math.floor(interval) + " hours";
+        }
+        interval = seconds / 60;
+        if (interval > 1) {
+            return Math.floor(interval) + " minutes";
+        }
+        return Math.floor(seconds) + " seconds";
+
+    }
+
   }

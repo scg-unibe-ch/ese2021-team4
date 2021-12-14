2e6aad9826adca286cb7223de83b82c7f5db9178
Lena Georgescu <lena.georgescu@students.unibe.ch>
2021-12-14 11:45:33 +0100

    Added possible formatting solution for fanshop

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/models/product.model.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/models/product.model.ts
@@ -19,7 +19,7 @@ export class Product {
     var titleLines = Math.min(2, Math.ceil(this.title.length / 100));
     var descriptionLines = Math.min(2 ,Math.ceil(this.noHtmlDescription.length / 100));
     var minimumRows = 1;
-    return titleLines + descriptionLines + minimumRows;
+    return 2;
   }
 
   removeTags(description : String) : String {
